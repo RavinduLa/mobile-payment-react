69d7b9dad68110bec75d8412b1ca36ebdfd0aad9
RavinduLa <ravinduwata2@gmail.com>
2021-05-05 23:51:48 +0530

    Added home and Payment

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Home extends React.Component{
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Home;
---
+++ src/components/Payment.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+
+class Payment extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default Payment;
